4360916817b85d4fc6fe819948cc10a8209a1620
nguyenrtm <nguyenrtm@gmail.com>
2025-05-16 16:09:37 +0700

    edit 10 seed run

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ The seeds are given at later time.
 - Version v1.1: Fix a small logic of `start_time` should less than `dead_line`
 
 # Testing scripts
-Full experiments on seed 2025 can be ran with
+Full experiments on seed 2025 can be ran with the following commmand:
 
 ```bash run_all.sh```
+
+Full experiments over 10 random seeds can be ran with the following commmand:
+
+```bash run_all_10_seeds.sh
---
+++ run_all_10_seeds.sh
@@ -0,0 +1,5 @@
+python main.py --seed 0 --max_time_steps 1000 --map map1.txt --num_agents 5 --n_packages 100
+python main.py --seed 0 --max_time_steps 1000 --map map2.txt --num_agents 5 --n_packages 100
+python main.py --seed 0 --max_time_steps 1000 --map map3.txt --num_agents 5 --n_packages 500
+python main.py --seed 0 --max_time_steps 1000 --map map4.txt --num_agents 10 --n_packages 500
+python main.py --seed 0 --max_time_steps 1000 --map map5.txt --num_agents 10 --n_packages 1000
